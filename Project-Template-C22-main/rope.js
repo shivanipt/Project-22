4070dcd5875ae9acdd918ed2f0cdaf13b27a6a49
shivanipt <87143211+shivanipt@users.noreply.github.com>
2021-09-10 15:32:16 -0500

    Add files via upload

--- Project-Template-C22-main/rope.js
+++ Project-Template-C22-main/rope.js
@@ -1,5 +1,5 @@
 class rope{
-	constructor()
+	constructor(bodyA, bodyB, pointA, pointB) 
 	{
 
 	//create rope constraint here
@@ -42,6 +42,16 @@ class rope{
 
 
     //create display() here 
-	rope1.display();
+	display() {
+		push();
+			strokeWeight(2);
+			stroke(225);
+			line(rope1.pointA.x,rope1.pointA.y,ball1.position.x,ball1.position.y);
+			line(rope2.pointA.x,rope2.pointA.y,ball2.position.x,ball2.position.y);
+			line(rope3.pointA.x,rope3.pointA.y,ball3.position.x,ball3.position.y);
+			line(rope4.pointA.x,rope4.pointA.y,ball4.position.x,ball4.position.y);
+			line(rope5.pointA.x,rope5.pointA.y,ball5.position.x,ball5.position.y);
+		pop();
 	
+	}
 }
--- Project-Template-C22-main/sketch.js
+++ Project-Template-C22-main/sketch.js
@@ -18,9 +18,9 @@ function setup() {
 	world = engine.world;
 
 	var ball_options = {
-		restitution: 0.8
-		isStatic = false
-		friction: 0.01
+		restitution: 0.8,
+		isStatic: false,
+		friction: 0.01,
 		density: 0.04
 	}
 
@@ -34,42 +34,6 @@ function setup() {
 	World.add(world, ball4);
 	ball5 = Bodies.circle(500, 50, 10, ball_options);
 	World.add(world, ball5);
-
-	rope1 = Matter.Constraint.create( {
-		pointA:{x:300, y:100},
-		bodyB:ball1,
-		pointB:{x:0, y:0},
-		length:100,
-		stiffness:0.1
-	});
-	rope2 = Matter.Constraint.create( {
-		pointA:{x:350, y:100},
-		bodyB:ball2,
-		pointB:{x:0, y:0},
-		length:100,
-		stiffness:0.1
-	});
-	rope3 = Matter.Constraint.create( {
-		pointA:{x:400, y:100},
-		bodyB:ball3,
-		pointB:{x:0, y:0},
-		length:100,
-		stiffness:0.1
-	});
-	rope4 = Matter.Constraint.create( {
-		pointA:{x:450, y:100},
-		bodyB:ball4,
-		pointB:{x:0, y:0},
-		length:100,
-		stiffness:0.1
-	});
-	rope5 = Matter.Constraint.create( {
-		pointA:{x:500, y:100},
-		bodyB:ball5,
-		pointB:{x:0, y:0},
-		length:100,
-		stiffness:0.1
-	});
 
 	var roof_options={
 		isStatic:true			
@@ -91,15 +55,21 @@ function draw() {
   rect(roof.position.x,roof.position.y,230,20);
 
   //call display() to show ropes here
-	push();
-	strokeWeight(2);
-	stroke(225);
-	line(rope1.pointA.x,rope1.pointA.y,ball1.position.x,ball1.position.y);
-	line(rope2.pointA.x,rope2.pointA.y,ball2.position.x,ball2.position.y);
-	line(rope3.pointA.x,rope3.pointA.y,ball3.position.x,ball3.position.y);
-	line(rope4.pointA.x,rope4.pointA.y,ball4.position.x,ball4.position.y);
-	line(rope5.pointA.x,rope5.pointA.y,ball5.position.x,ball5.position.y);
-	pop();
+  	rope1.display();
+  	rope2.display();
+  	rope3.display();
+  	rope4.display();
+  	rope5.display();
+
+	//push();
+	//strokeWeight(2);
+	//stroke(225);
+	//line(rope1.pointA.x,rope1.pointA.y,ball1.position.x,ball1.position.y);
+	//line(rope2.pointA.x,rope2.pointA.y,ball2.position.x,ball2.position.y);
+	//line(rope3.pointA.x,rope3.pointA.y,ball3.position.x,ball3.position.y);
+	//line(rope4.pointA.x,rope4.pointA.y,ball4.position.x,ball4.position.y);
+	//line(rope5.pointA.x,rope5.pointA.y,ball5.position.x,ball5.position.y);
+	//pop();
   
   //create ellipse shape for multiple bobs here
   ellipse(ball1.position.x, ball1.position.y, 10);
